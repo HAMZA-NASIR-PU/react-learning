6280731148216992db704c93073f2b49c15d1d71
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-09-10 11:20:26 +0500

    Solve project structure issue

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,38 +1,6 @@
-import reactImg from './assets/react-core-concepts.png'  //Build process will handle tis during making the bundle for deployment.
-//Now the image is a javascript object.
-import componentsImg from './assets/components.png'
 import { CORE_CONCEPTS } from './data';
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />  This is not a good way of using image bevause we lost the image during deployment*/}
-      <img src={reactImg} alt='Stylized atom' />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-//object destructuring
-//spread operator
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from './components/Heder/Header';
+import CoreConcept from './components/CoreConcept';
 
 function App() {
   return (
---
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,12 @@
+
+//object destructuring
+//spread operator
+export default function CoreConcept({ image, title, description }) {
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    );
+}
